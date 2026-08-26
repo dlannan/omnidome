51f4391ccabe9df4a5174be86162795bd2f105df
Kakutai <dlannan68@gmail.com>
2026-08-26 20:42:03 +0930

    Better pathing.

--- omnidome_do_build.bat
+++ omnidome_do_build.bat
@@ -1,3 +1,3 @@
 "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" ^
-  --build D:\work\calytrix\tools\omnidome-build-msvc2017 ^
+  --build ..\omnidome_build_VS2017 ^
   --config Release
--- omnidome_do_config.bat
+++ omnidome_do_config.bat
@@ -1,6 +1,6 @@
 "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" ^
-  -S D:\work\calytrix\tools\omnidome-master ^
-  -B D:\work\calytrix\tools\omnidome-build-msvc2017 ^
+  -S . ^
+  -B ..\omnidome_build_VS2017 ^
   -G "Visual Studio 15 2017" ^
   -A x64 ^
   -DCMAKE_PREFIX_PATH=C:\Qt\Qt5.14.2\5.14.2\msvc2017_64
